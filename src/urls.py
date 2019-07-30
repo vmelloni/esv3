a2a81ea355aaaab07016fd54396a6a1cde20c7f2
jllorente24 <jessicallorente@gmail.com>
2019-07-29 21:08:11 -0300

    Update urls.py

--- src/urls.py
+++ src/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 
-    "V1 - prueba git
+    V1 - prueba git
 
 """
 from django.contrib import admin
